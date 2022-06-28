7c36cb7fa77a7d8c293425f3333812891b58d85a
vkotaru <prasanth.kotaru@gmail.com>
2022-06-28 00:47:28 -0700

    Consolidate headers into a single header

---
+++ include/nonlinear_controls.h
@@ -0,0 +1,8 @@
+#ifndef _NONLINEAR_CONTROLS_H_
+#define _NONLINEAR_CONTROLS_H_
+
+#include "common/utils.hpp"
+#include "controls/linear_mpc.h"
+#include "data_types/data_types.hpp"
+
+#endif //_NONLINEAR_CONTROLS_H_
